6e8539aa69f2040aefebcfdecedb3eda3bf02203
armanforghani <armanforghani@gmail.com>
2015-09-28 17:44:31 +0330

    Fixed some issues for RTL

--- spectrum-rtl.js
+++ spectrum-rtl.js
@@ -18,6 +18,19 @@
 })(function($, undefined) {
     "use strict";
 
+    // Popular large color palette
+    var defaultPalette = [
+            // ['black', 'rgb(255, 128, 0);', 'hsv 100 70 50', 'rgba(20, 30, 40, 0.6)']
+            ["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)", "rgb(153, 153, 153)", "rgb(183, 183, 183)", "rgb(204, 204, 204)", "rgb(217, 217, 217)", "rgb(239, 239, 239)", "rgb(243, 243, 243)", "rgb(255, 255, 255)"],
+            ["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"],
+            ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)", "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)"],
+            ["rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)", "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)"],
+            ["rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)", "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)"],
+            ["rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)", "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)"],
+            ["rgb(133, 32, 12)", "rgb(153, 0, 0)", "rgb(180, 95, 6)", "rgb(191, 144, 0)", "rgb(56, 118, 29)", "rgb(19, 79, 92)", "rgb(17, 85, 204)", "rgb(11, 83, 148)", "rgb(53, 28, 117)", "rgb(116, 27, 71)"],
+            ["rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)", "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
+    ];
+    
     var defaultOpts = {
 
         // Callbacks
@@ -30,32 +43,32 @@
         // Options
         color: false,
         flat: false,
-        showInput: false,
+        showInput: true,
         allowEmpty: false,
         showButtons: true,
         clickoutFiresChange: true,
-        showInitial: false,
-        showPalette: false,
+        showInitial: true,
+        showPalette: true,
         showPaletteOnly: false,
         hideAfterPaletteSelect: false,
         togglePaletteOnly: false,
         showSelectionPalette: true,
         localStorageKey: false,
         appendTo: "body",
-        maxSelectionSize: 7,
-        cancelText: "cancel",
-        chooseText: "choose",
+        maxSelectionSize: 10,
+        cancelText: "انصراف",
+        chooseText: "تایید",
         togglePaletteMoreText: "more",
         togglePaletteLessText: "less",
         clearText: "Clear Color Selection",
         noColorSelectedText: "No Color Selected",
-        preferredFormat: false,
+        preferredFormat: "hex6",
         className: "", // Deprecated - use containerClassName and replacerClassName instead.
         containerClassName: "",
         replacerClassName: "",
-        showAlpha: false,
+        showAlpha: true,
         theme: "sp-light",
-        palette: [["#ffffff", "#000000", "#ff0000", "#ff8000", "#ffff00", "#008000", "#0000ff", "#4b0082", "#9400d3"]],
+        palette: defaultPalette, //[["#ffffff", "#000000", "#ff0000", "#ff8000", "#ffff00", "#008000", "#0000ff", "#4b0082", "#9400d3"]],
         selectionPalette: [],
         disabled: false,
         offset: null
@@ -984,10 +997,13 @@
         var offset = input.offset();
         offset.top += inputHeight;
 
-        offset.left -=
-            Math.min(offset.left, (offset.left + dpWidth > viewWidth && viewWidth > dpWidth) ?
-            Math.abs(offset.left + dpWidth - viewWidth) : 0);
+        //offset.left -=
+        //    Math.min(offset.left, (offset.left + dpWidth > viewWidth && viewWidth > dpWidth) ?
+        //    Math.abs(offset.left + dpWidth - viewWidth) : 0);
 
+		// RTL
+        offset.left = Math.max(offset.left - dpWidth + input.outerWidth(), 0);
+		
         offset.top -=
             Math.min(offset.top, ((offset.top + dpHeight > viewHeight && viewHeight > dpHeight) ?
             Math.abs(dpHeight + inputHeight - extraY) : extraY));
@@ -1394,6 +1410,11 @@
             }
             if (format === "hsv") {
                 formattedString = this.toHsvString();
+            }
+			
+            // Use rgba format only if has Alpha
+            if (hasAlpha) {
+                return this.toRgbString();
             }
 
             return formattedString || this.toHexString();
